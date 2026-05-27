4be9767fee50945e1acdbcf37c99eee690e77f8e
Himani <himanivaghasiya19.hv@gmail.com>
2026-05-27 02:49:29 -0700

    Fix case mismatch for AccountSetting

--- rental/src/components/Accountsetting.jsx
+++ rental/src/components/Accountsetting.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "./AccountSetting.css";
+import "./Accountsetting.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
